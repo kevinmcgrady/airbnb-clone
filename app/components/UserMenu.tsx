9c8857cbf058e99cc9f97c460f5d0236742733cc
Kev Mcgrady <kevinmcgrady47@gmail.com>
2023-06-27 11:38:33 +0100

    feat/favorites page

--- app/components/UserMenu.tsx
+++ app/components/UserMenu.tsx
@@ -62,7 +62,11 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                   onClick={() => router.push('/trips')}
                   label='My trips'
                 />
-                <MenuItem onClick={() => {}} label='My favorites' />
+                <MenuItem
+                  onClick={() => router.push('/favorites')}
+                  label='My favorites'
+                />
+                <MenuItem onClick={() => {}} label='My Properties' />
                 <MenuItem
                   onClick={() => router.push('/reservations')}
                   label='My reservations'
